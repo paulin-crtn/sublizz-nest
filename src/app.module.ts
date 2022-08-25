50f09d7e1dd7727483f2c2a011663f300dcfc18d
Paulin Caratini <41628154+paulin-crtn@users.noreply.github.com>
2022-08-25 14:40:55 +0200

    feat(configModule)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,18 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from './prisma/prisma.module';
-import { LeaseModule } from './lease/lease.module';
-import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { LeaseModule } from './lease/lease.module';
 
 @Module({
-  imports: [PrismaModule, LeaseModule, UserModule, AuthModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    PrismaModule,
+    AuthModule,
+    UserModule,
+    LeaseModule,
+  ],
   controllers: [],
   providers: [],
 })
--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,13 +1,15 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
+import { ConfigService } from '@nestjs/config';
+import { config } from 'process';
 
 @Injectable()
 export class PrismaService extends PrismaClient {
-  constructor() {
+  constructor(configService: ConfigService) {
     super({
       datasources: {
         db: {
-          url: 'postgresql://postgres:eefVK@B7GKyC8BD@db.unnpkjktcopeemblwjer.supabase.co:5432/postgres',
+          url: configService.get('DATABASE_URL'),
         },
       },
     });
