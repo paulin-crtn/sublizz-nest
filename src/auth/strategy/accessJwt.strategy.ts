cbc1d0a0a12bb04cec60f35f8f9422a6e53a1731
Paulin Caratini <41628154+paulin-crtn@users.noreply.github.com>
2022-09-12 09:52:19 +0200

    feat(strategy): emailVerifiedAt

--- src/auth/strategy/accessJwt.strategy.ts
+++ src/auth/strategy/accessJwt.strategy.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
@@ -21,9 +21,19 @@ export class AccessJwtStrategy extends PassportStrategy(
   }
 
   async validate(payload: { sub: number; email: string }) {
+    // Find the user by id
     const user = await this.prismaService.user.findUnique({
       where: { id: payload.sub },
     });
+    // If user does not exist throw exception
+    if (!user) {
+      throw new UnauthorizedException('User does not exist.');
+    }
+    // If user's email is not verified throw exception
+    if (!user.emailVerifiedAt) {
+      throw new UnauthorizedException('Email must be verified.');
+    }
+    // Return user
     delete user.passwordHash;
     delete user.refreshTokenHash;
     return user;
