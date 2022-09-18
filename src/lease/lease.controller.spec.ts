477659a0da78391c9e79befd0512379c7178d470
Paulin Caratini <41628154+paulin-crtn@users.noreply.github.com>
2022-09-18 10:46:31 +0200

    feat(lease): set test

--- src/lease/lease.controller.spec.ts
+++ src/lease/lease.controller.spec.ts
@@ -1,15 +1,24 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { LeaseController } from './lease.controller';
+import { LeaseService } from './lease.service';
+
+const mockLeaseService = {};
 
 describe('LeaseController', () => {
   let controller: LeaseController;
+  let service: LeaseService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [LeaseController],
-    }).compile();
+      providers: [LeaseService],
+    })
+      .overrideProvider(LeaseService)
+      .useValue(mockLeaseService)
+      .compile();
 
     controller = module.get<LeaseController>(LeaseController);
+    service = module.get<LeaseService>(LeaseService);
   });
 
   it('should be defined', () => {
