f86ecfdfcadc879d41fe5960766aefcf7f252c9e
Paulin Caratini <41628154+paulin-crtn@users.noreply.github.com>
2022-09-19 12:17:36 +0200

    feat(strategy): use relative import

--- src/auth/strategy/refreshJwt.strategy.ts
+++ src/auth/strategy/refreshJwt.strategy.ts
@@ -2,7 +2,7 @@ import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
-import { PrismaService } from 'src/prisma/prisma.service';
+import { PrismaService } from '../../prisma/prisma.service';
 import * as argon from 'argon2';
 
 @Injectable()
