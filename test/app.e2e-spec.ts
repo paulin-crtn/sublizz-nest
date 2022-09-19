6426cbf2bbca0269b1db559461f71683d9294881
Paulin Caratini <41628154+paulin-crtn@users.noreply.github.com>
2022-09-19 12:18:24 +0200

    feat(test): e2e

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,24 +1,62 @@
+/* -------------------------------------------------------------------------- */
+/*                                   IMPORTS                                  */
+/* -------------------------------------------------------------------------- */
 import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
-import * as request from 'supertest';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
+import * as pactum from 'pactum';
 import { AppModule } from './../src/app.module';
+import { PrismaService } from '../src/prisma/prisma.service';
+import { SignUpDto } from '../src/auth/dto';
 
+/* -------------------------------------------------------------------------- */
+/*                                  MAIN TEST                                 */
+/* -------------------------------------------------------------------------- */
 describe('AppController (e2e)', () => {
   let app: INestApplication;
+  let prismaService: PrismaService;
 
-  beforeEach(async () => {
-    const moduleFixture: TestingModule = await Test.createTestingModule({
+  /* ------------------------------ CONFIGURATION ----------------------------- */
+  beforeAll(async () => {
+    // Create App
+    const moduleRef: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
 
-    app = moduleFixture.createNestApplication();
+    app = moduleRef.createNestApplication();
+    app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
     await app.init();
+    await app.listen(process.env.APP_PORT); // Needed for pactum
+
+    // Clean DB
+    prismaService = app.get(PrismaService);
+    await prismaService.cleanDb();
+
+    // Pactum
+    pactum.request.setBaseUrl(
+      process.env.APP_DOMAIN + ':' + process.env.APP_PORT,
+    );
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+  afterAll(() => {
+    app.close();
+  });
+
+  describe('Authentication', () => {
+    describe('POST /auth/signup', () => {
+      const dto: SignUpDto = {
+        firstName: 'Pascal',
+        email: 'email@mail.com',
+        password: 'password',
+      };
+      it('should signup a user', () => {
+        return pactum
+          .spec()
+          .post('/auth/signup')
+          .withBody(dto)
+          .expectStatus(201);
+      });
+    });
+
+    describe('POST /auth/signin', () => {});
   });
 });
